2d72f1110836ab3f4b862f098fcc29628f857a26
LucasNG <84586768+LucasDNG@users.noreply.github.com>
2026-02-01 17:54:37 -0300

    hola amore port 80

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
-app.get('/products', (req,res)=>{
-    res.json({
-        id: '1',
-        name: "laptop-gaming",
-        price: 1000,
-        description: "laptop gaming lenovo",
-        imageURL: "https://p3-ofp.static.pub/fes/cms/2022/09/29/bp2i817wo5lsv7wm7xddnflh9tm7d7225910.png"
-
-    })
+app.get('/products', (req, res) => {
+  res.json({
+    id: '1',
+    name: "laptop-gaming",
+    price: 1000,
+    description: "laptop gaming lenovo",
+    imageURL: "https://p3-ofp.static.pub/fes/cms/2022/09/29/bp2i817wo5lsv7wm7xddnflh9tm7d7225910.png"
+  })
 })
 
-
-app.listen(port, ()=>{
-    console.log('server on port', port)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`)
 })
