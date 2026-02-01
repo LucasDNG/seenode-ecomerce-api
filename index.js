e7b66da6716b005d79d26d15f5379e011d16126c
LucasNG <84586768+LucasDNG@users.noreply.github.com>
2026-02-01 19:07:24 -0300

    hola amore port 80 segundo intento

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.status(200).send('OK')
@@ -15,6 +15,6 @@ app.get('/products', (req, res) => {
   })
 })
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
